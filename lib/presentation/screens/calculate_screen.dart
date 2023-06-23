85a11990db68ee6c45d3b8cc584c0aabc624dd3a
jhongo <jhon_gom22@outlook.com>
2023-06-22 23:28:15 -0500

    Progress in CustomPainter

--- lib/presentation/screens/calculate_screen.dart
+++ lib/presentation/screens/calculate_screen.dart
@@ -11,12 +11,50 @@ class CalculateScreen extends StatelessWidget {
       backgroundColor: Color(0xFF210535),
       body: Stack(
         children: [
-          Image.asset('assets/imgs/astronaut-in-space.png')
+          CustomHeader()
+          // Image.asset('assets/imgs/astronaut-in-space.png')
         ],
       )
     );
   }
 }
+
+class CustomHeader extends StatelessWidget {
+  const CustomHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width:MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: CustomPaint(
+        painter:HeaderPainter(),
+      ),
+
+    );
+  }
+}
+
+class HeaderPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+  
+  final paint = Paint();
+  paint.color = Color(0xFFfefae0);
+  paint.style = PaintingStyle.stroke;
+  paint.strokeWidth = 5;
+
+  final path = Path();
+  // path.lin
+    
+canvas.drawPath(path, paint);
+}
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+
+}
+
 
 // HEX CODES: 
 // #210535
